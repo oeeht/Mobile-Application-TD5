fea22e5e8541e024d9c9db484eee20c0233dc8cd
oeeht <theocorin@gmail.com>
2016-02-26 16:42:55 +0100

    first commit

---
+++ MobileApplicationTD5/app/src/main/java/com/esilv/theo/matd5/activities/YTConnector.java
@@ -0,0 +1,66 @@
+package com.esilv.theo.matd5.activities;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.esilv.theo.matd5.R;
+import com.esilv.theo.matd5.models.VideoYT;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class YTConnector {
+    private YouTube youtube;
+    private YouTube.Search.List query;
+
+    // Your developer key goes here
+    public static final String KEY = "AIzaSyBnoHm5KxaJiOYPZN8QctA5h9hrcBv9wxQ";
+
+    public YTConnector(Context content) {
+        youtube = new YouTube.Builder(new NetHttpTransport(),
+                new JacksonFactory(), new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest hr) throws IOException {}
+        }).setApplicationName(content.getString(R.string.app_name)).build();
+
+        try{
+            query = youtube.search().list("id,snippet");
+            query.setKey(KEY);
+            query.setType("video");
+            query.setMaxResults((long)20);
+            query.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
+        }catch(IOException e){
+            Log.d("YC", "Could not initialize: " + e);
+        }
+    }
+
+    public List<VideoYT> search(String keywords){
+        query.setQ(keywords);
+        try{
+            SearchListResponse response = query.execute();
+            List<SearchResult> results = response.getItems();
+
+            List<VideoYT> items = new ArrayList<VideoYT>();
+            for(SearchResult result:results){
+                VideoYT item = new VideoYT();
+                item.setTitle(result.getSnippet().getTitle());
+                item.setDescription(result.getSnippet().getDescription());
+                item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
+                item.setId(result.getId().getVideoId());
+                items.add(item);
+            }
+            return items;
+        }catch(IOException e){
+            Log.d("YC", "Could not search: "+e);
+            return null;
+        }
+    }
+}
